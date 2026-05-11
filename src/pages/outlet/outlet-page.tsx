3936dc7c9d97e158cf57929480c9bc53f31435f3
Pedro Souza <pedrosport10@gmail.com>
2026-05-11 19:29:23 -0300

    feat: update translation files for English and Spanish, add social media icons to OutletPage component

--- src/pages/outlet/outlet-page.tsx
+++ src/pages/outlet/outlet-page.tsx
@@ -1,15 +1,7 @@
 import { Dock, DockIcon } from "@/components/ui/dock"
 import { ScrollProgress } from "@/components/ui/scroll-progress"
 import ReactLenis from "lenis/react"
-import {
-  CircleUser,
-  Download,
-  FolderGit2,
-  Home,
-  Library,
-  MoveLeft,
-  Zap,
-} from "lucide-react"
+import { Download, Home, MoveLeft, Zap } from "lucide-react"
 import { Outlet, useNavigate } from "react-router"
 import { motion } from "motion/react"
 import {
@@ -31,6 +23,8 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { GithubIcon } from "@/components/icons/GithubIcon"
+import { LinkedinIcon } from "@/components/icons/LinkedinIcon"
 
 const OutletPage = () => {
   const navigate = useNavigate()
@@ -45,17 +39,9 @@ const OutletPage = () => {
     navigate(RoutesUrl.HOME)
   }, [navigate])
 
-  const handleGoToAbout = useCallback(() => {
-    navigate(RoutesUrl.ABOUT)
-  }, [navigate])
-
-  const handleGoToExperiences = useCallback(() => {
-    navigate(RoutesUrl.EXPERIENCIES)
-  }, [navigate])
-
-  const handleGoToProjects = useCallback(() => {
-    navigate(RoutesUrl.PROJECTS)
-  }, [navigate])
+  const handleGoToGithub = useCallback(() => {
+    window.open("https://github.com/iShouldz", "_blank")
+  }, [])
 
   const handleGoToLinkedIn = useCallback(() => {
     window.open("https://www.linkedin.com/in/pedro-souza-385794241/", "_blank")
@@ -121,40 +107,6 @@ const OutletPage = () => {
             </Tooltip>
           </DockIcon>
 
-          <DockIcon onClick={handleGoToExperiences}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Library />
-              </TooltipTrigger>
-
-              <TooltipContent>
-                <p>{t("dock.experiences")}</p>
-              </TooltipContent>
-            </Tooltip>
-          </DockIcon>
-
-          <DockIcon onClick={handleGoToProjects}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <FolderGit2 />
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{t("dock.projects")}</p>
-              </TooltipContent>
-            </Tooltip>
-          </DockIcon>
-
-          <DockIcon onClick={handleGoToAbout}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <CircleUser />
-              </TooltipTrigger>
-              <TooltipContent>
-                <p>{t("dock.about")}</p>
-              </TooltipContent>
-            </Tooltip>
-          </DockIcon>
-
           <DockIcon>
             <Tooltip>
               <a href="/Curriculo.pdf" download="Curriculo.pdf">
@@ -169,19 +121,22 @@ const OutletPage = () => {
             </Tooltip>
           </DockIcon>
 
+          <DockIcon onClick={handleGoToGithub}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <GithubIcon />
+              </TooltipTrigger>
+
+              <TooltipContent>
+                <p>{t("dock.github")}</p>
+              </TooltipContent>
+            </Tooltip>
+          </DockIcon>
+
           <DockIcon onClick={handleGoToLinkedIn}>
             <Tooltip>
               <TooltipTrigger asChild>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  className="bi bi-linkedin"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-                </svg>
+                <LinkedinIcon />
               </TooltipTrigger>
               <TooltipContent>
                 <p>{t("dock.linkedin")}</p>
